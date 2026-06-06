3b19c0d847127fba1b5201326202d6d3e87d16b0
hazaeltc1210 <hazaeltc1210@gmail.com>
2026-06-05 20:29:23 -0500

    tarea 4

---
+++ tarea4.js
@@ -0,0 +1,13 @@
+console.log("Números pares del 1 al 20:");
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+console.log("Números impares del 1 al 20:");
+for (let i = 1; i <= 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
