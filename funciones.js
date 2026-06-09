260664a756f696f5c6477fe4be7ad6c90bfdf308
hazaeltc1210 <hazaeltc1210@gmail.com>
2026-06-08 21:16:09 -0500

    Evento, resta, multiplicar y dividir

--- funciones.js
+++ funciones.js
@@ -2,6 +2,12 @@ const num1 = document.querySelector("#numero_1")
 const num2 = document.querySelector("#numero_2")
 const buttonSuma = document.querySelector
 ("#btnSuma")
+const buttonResta = document.querySelector
+("#btnResta")
+const buttonMultiplica = document.querySelector
+("#btnMultiplica")
+const buttonDivide = document.querySelector
+("#btnDivide")
 const resultado = document.querySelector
 ("#resultado")
 
@@ -11,12 +17,20 @@ buttonSuma.addEventListener("click", () =>{
    limpiar(num1, num2)
 })
 
-//tarea1: agregar eventos a los botones de resta, multiplicación y división, y mostrar el resultado en el elemento con id resultado. No olvidar limpiar los campos después de cada operación.
-//tarea2: crear un archivo que se llame metodos.html, contendra unicamente un input de tipo numero y un boton que calculen: numero pares, impares y primos
-//Que el resultado se muestre en un parrafo debajo del boton ejemplo:
-//Fecha de entrega: jueves 11 de junio hasta las 11:59 pm
-//1- clic al boton  ---> el numero es impar y primo 
-//2- clic al boton  ---> el numero es impar y primo 
-//3- clic al boton  ---> el numero es impar y primo 
-//4- clic al boton  ---> el numero es impar y primo 
-//5- clic al boton  ---> el numero es impar y primo 
+buttonResta.addEventListener("click", () =>{
+   const res = resta(num1.value, num2.value)
+   resultado.textContent = res
+   limpiar(num1, num2)
+})
+
+buttonMultiplica.addEventListener("click", () =>{
+   const res = multiplica(num1.value, num2.value)
+   resultado.textContent = res
+   limpiar(num1, num2)
+})
+
+buttonDivide.addEventListener("click", () =>{
+   const res = divide(num1.value, num2.value)
+   resultado.textContent = res
+   limpiar(num1, num2)
+})
